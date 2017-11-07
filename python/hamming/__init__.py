3818579ce9bc6bf8dddce8cfe072cfc4214f209f
Michael Murray <michael@planetmurray.org>
2017-11-07 14:44:21 -0800

    passing tests

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,10 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+
+def compute(s1, s2):
+
+    if len(s1) != len(s2):
+        raise ValueError("Length of s1 != length of s2, invalid data...")
+
+    return sum(c1 != c2 for c1, c2 in zip(s1, s2))
